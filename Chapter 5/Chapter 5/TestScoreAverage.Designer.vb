9811d6e5a6b37ff1c134777860743b0d12ec2aa7
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-11-29 10:36:15 -0600

    Added Partial Test Score Average

---
+++ Chapter 5/Chapter 5/TestScoreAverage.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TestScoreAverage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblTotalAverage = New System.Windows.Forms.Label()
+        Me.btnScores = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Test Average:"
+        '
+        'lblTotalAverage
+        '
+        Me.lblTotalAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalAverage.Location = New System.Drawing.Point(127, 52)
+        Me.lblTotalAverage.Name = "lblTotalAverage"
+        Me.lblTotalAverage.Size = New System.Drawing.Size(134, 26)
+        Me.lblTotalAverage.TabIndex = 1
+        '
+        'btnScores
+        '
+        Me.btnScores.Location = New System.Drawing.Point(28, 118)
+        Me.btnScores.Name = "btnScores"
+        Me.btnScores.Size = New System.Drawing.Size(95, 36)
+        Me.btnScores.TabIndex = 2
+        Me.btnScores.Text = "Get Scores"
+        Me.btnScores.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(168, 118)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(76, 36)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'TestScoreAverage
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 184)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnScores)
+        Me.Controls.Add(Me.lblTotalAverage)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "TestScoreAverage"
+        Me.Text = "Test Score Average"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblTotalAverage As System.Windows.Forms.Label
+    Friend WithEvents btnScores As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+End Class
---
+++ Chapter 5/Chapter 5/TestScoreAverage.vb
@@ -0,0 +1,41 @@
+﻿Public Class TestScoreAverage
+
+    Private Sub btnScores_Click(sender As Object, e As EventArgs) Handles btnScores.Click
+        ' This procedure gets the test scores, then calculates and 
+        ' displays the average.
+        Dim intNumScores As Integer = 5             ' The number of test scores
+        Dim dblTestScore As Double                  ' To hold a test score
+        Dim dblTotal As Double = 0                  ' Accumulator, intialized to 0
+        Dim dblAverage As Double                    ' The average of test scores
+        Dim strInput As String                      ' To hold user input    
+        Dim intCount As Integer = 1                 ' Counter variable, intialized to 1
+
+        ' Promt the user for the number of  test scores.
+        strInput = InputBox("How many test scores do you want to average?")
+
+        ' Convert the input to an integer.
+        If Integer.TryParse(strInput, intNumScores) Then
+
+            ' Get the test scores.
+            Do Until intCount > intNumScores
+
+                'Prompt the user for a score.
+                strInput = InputBox("Enter a test score " & intCount.ToString())
+
+                ' Convert the input to a double.
+                If Double.TryParse(strInput, dblTestScore) Then
+
+                    ' Add the score to the accumulator. 
+                    dblTotal += dblTestScore
+
+                    ' Add 1 to the counter. 
+                    intCount += 1
+
+                    ' Invalid test score. 
+                    MessageBox.Show("Enter a numeric test score")
+                End If
+            Loop
+
+        End If
+    End Sub
+End Class
