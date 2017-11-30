f12cef380408e20214988113054b52e4fe4508d1
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-11-30 10:44:27 -0600

    Added ComboBoxDemoProgram

---
+++ ComboBoxDemo/ComboBoxDemo/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cboCountries = New System.Windows.Forms.ComboBox()
+        Me.cboPlays = New System.Windows.Forms.ComboBox()
+        Me.cboArtists = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtCountry = New System.Windows.Forms.TextBox()
+        Me.txtPlay = New System.Windows.Forms.TextBox()
+        Me.txtArtist = New System.Windows.Forms.TextBox()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cboCountries
+        '
+        Me.cboCountries.FormattingEnabled = True
+        Me.cboCountries.Items.AddRange(New Object() {"England ", "Ireland ", "Austrailia", ""})
+        Me.cboCountries.Location = New System.Drawing.Point(110, 43)
+        Me.cboCountries.Name = "cboCountries"
+        Me.cboCountries.Size = New System.Drawing.Size(121, 21)
+        Me.cboCountries.TabIndex = 0
+        '
+        'cboPlays
+        '
+        Me.cboPlays.FormattingEnabled = True
+        Me.cboPlays.Items.AddRange(New Object() {"Hamlet", "Romeo and Juliet "})
+        Me.cboPlays.Location = New System.Drawing.Point(110, 90)
+        Me.cboPlays.Name = "cboPlays"
+        Me.cboPlays.Size = New System.Drawing.Size(121, 21)
+        Me.cboPlays.TabIndex = 1
+        '
+        'cboArtists
+        '
+        Me.cboArtists.FormattingEnabled = True
+        Me.cboArtists.Items.AddRange(New Object() {"Da Vinci", "MichaelAngelo", "William Shakespeare"})
+        Me.cboArtists.Location = New System.Drawing.Point(110, 132)
+        Me.cboArtists.Name = "cboArtists"
+        Me.cboArtists.Size = New System.Drawing.Size(121, 21)
+        Me.cboArtists.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Choose a country:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Choose a play:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 135)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Choose an artist:"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtArtist)
+        Me.GroupBox1.Controls.Add(Me.txtPlay)
+        Me.GroupBox1.Controls.Add(Me.txtCountry)
+        Me.GroupBox1.Location = New System.Drawing.Point(287, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(265, 165)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Your Choices"
+        '
+        'txtCountry
+        '
+        Me.txtCountry.Location = New System.Drawing.Point(15, 31)
+        Me.txtCountry.Name = "txtCountry"
+        Me.txtCountry.Size = New System.Drawing.Size(244, 20)
+        Me.txtCountry.TabIndex = 0
+        '
+        'txtPlay
+        '
+        Me.txtPlay.Location = New System.Drawing.Point(15, 78)
+        Me.txtPlay.Name = "txtPlay"
+        Me.txtPlay.Size = New System.Drawing.Size(244, 20)
+        Me.txtPlay.TabIndex = 1
+        '
+        'txtArtist
+        '
+        Me.txtArtist.Location = New System.Drawing.Point(15, 120)
+        Me.txtArtist.Name = "txtArtist"
+        Me.txtArtist.Size = New System.Drawing.Size(244, 20)
+        Me.txtArtist.TabIndex = 2
+        '
+        'btnShow
+        '
+        Me.btnShow.Location = New System.Drawing.Point(145, 211)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(74, 39)
+        Me.btnShow.TabIndex = 7
+        Me.btnShow.Text = "Show Selections"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(253, 211)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(74, 39)
+        Me.btnReset.TabIndex = 8
+        Me.btnReset.Text = "Reset Form"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(359, 211)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(74, 39)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(587, 262)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnShow)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboArtists)
+        Me.Controls.Add(Me.cboPlays)
+        Me.Controls.Add(Me.cboCountries)
+        Me.Name = "Form1"
+        Me.Text = "Combo Box Demo"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cboCountries As System.Windows.Forms.ComboBox
+    Friend WithEvents cboPlays As System.Windows.Forms.ComboBox
+    Friend WithEvents cboArtists As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtArtist As System.Windows.Forms.TextBox
+    Friend WithEvents txtPlay As System.Windows.Forms.TextBox
+    Friend WithEvents txtCountry As System.Windows.Forms.TextBox
+    Friend WithEvents btnShow As System.Windows.Forms.Button
+    Friend WithEvents btnReset As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ ComboBoxDemo/ComboBoxDemo/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        txtCountry.Text = cboCountries.Text
+        txtPlay.Text = cboPlays.Text
+        txtArtist.Text = cboArtists.Text
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
