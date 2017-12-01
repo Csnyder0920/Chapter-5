412cde647fd2f3c77d79c004dee5e50cf5226e2b
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-12-01 10:15:17 -0600

    Added ForNextLoop

---
+++ Next Loop Demo (Hat)/Next Loop Demo (Hat)/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.picSpinnerHat = New System.Windows.Forms.PictureBox()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.picSpinnerHat, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picSpinnerHat
+        '
+        Me.picSpinnerHat.Image = CType(resources.GetObject("picSpinnerHat.Image"), System.Drawing.Image)
+        Me.picSpinnerHat.Location = New System.Drawing.Point(12, 12)
+        Me.picSpinnerHat.Name = "picSpinnerHat"
+        Me.picSpinnerHat.Size = New System.Drawing.Size(166, 155)
+        Me.picSpinnerHat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picSpinnerHat.TabIndex = 0
+        Me.picSpinnerHat.TabStop = False
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(206, 207)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(80, 49)
+        Me.btnGo.TabIndex = 1
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(307, 207)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(80, 49)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(575, 268)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.picSpinnerHat)
+        Me.Name = "Form1"
+        Me.Text = "For Next Demo "
+        CType(Me.picSpinnerHat, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents picSpinnerHat As System.Windows.Forms.PictureBox
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Next Loop Demo (Hat)/Next Loop Demo (Hat)/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim intCount As Integer      ' Loop counter
+
+        ' Move the image across the form.
+        For intCount = 16 To 328
+            picSpinnerHat.Left = intCount
+        Next
+        For intCount = 16 To 328
+        Next
+        For intCount = 16 To 328
+        Next
+        For intCount = 16 To 328
+        Next
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
