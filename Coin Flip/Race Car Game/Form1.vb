a0e8c2c2adfa0097668262ecdb1334f6021ffe5a
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-12-01 11:32:53 -0600

    Added partial programs

--- Coin Flip/Race Car Game/Form1.vb
+++ Coin Flip/Race Car Game/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class Form1
+﻿Public Class Race_Car
 
+    Private Sub btnRace_Click(sender As Object, e As EventArgs) Handles btnRace.Click
+        Randomize()
+        picCar1.Left += Rnd() * 2
+        picCar2.Left += Rnd() * 4
+
+        picCar3.Left += Rnd() * 3
+        picCar4.Left += Rnd() * 3
+    End Sub
 End Class
--- Coin Flip/Race Car Game/My Project/Application.Designer.vb
+++ Coin Flip/Race Car Game/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Race_Car_Game.Form1
+            Me.MainForm = Global.Race_Car_Game.Race_Car
         End Sub
     End Class
 End Namespace
